ac348d3c33541869f1ce1a8639144834a2c6c9db
Giulio Starace <giulio.starace@gmail.com>
2018-12-05 23:31:45 +0000

    start organizing html docs for various web app pages - use jinja templates for scalability

--- pellicola.py
+++ pellicola.py
@@ -1,21 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 #<section>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Routes~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # website home page
 @app.route("/")
 def home():
-    return "Home Page"
+    return render_template('home.html')
 
 # where personal profile is maintained with movie ratings
 @app.route("/profile")
 def profile():
-    return "personal profile"
+    return render_template('profile.html', title = 'Profile')
 
 # where movie reccomendations will be handled
 @app.route("/reccomend")
 def reccomend():
-    return "recommendations page"
+    return render_template('recommend.html', title = 'For You')
 #</section> End of Routes
 
 # to allow server to be run with "python pellicola.py" -- no need for ENV vars
