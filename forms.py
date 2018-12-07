cda2a1bb617e95428282593e2d5d3324476ddd82
Giulio Starace <giulio.starace@gmail.com>
2018-12-07 15:19:54 +0000

    create forms.py where we define various form classes

---
+++ forms.py
@@ -0,0 +1,33 @@
+#<section>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Imports~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from flask_wtf import FlaskForm
+from wt_forms import StringField, PasswordField, SubmitField, BooleanField
+from wt_forms.validators import DataRequired, Length, Email, EqualTo
+#</section> End of Imports
+
+#<section>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Classes~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class RegistrationForm(FlaskForm):
+    """
+    Defines the required fields and validators for a registration form instance
+    """
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo(password)])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    """
+    Defines the required fields and validators for a login form instance
+    """
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+#</section> End of Classes
