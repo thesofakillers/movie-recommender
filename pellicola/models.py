785963a80a0cad7336393f2e20045292e0d9af12
Giulio Starace <giulio.starace@gmail.com>
2018-12-09 19:30:02 +0000

    got rid of image attribute in user

--- pellicola/models.py
+++ pellicola/models.py
@@ -20,8 +20,6 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=True)
     email = db.Column(db.String(254), unique=True, nullable=True)
-    image_file = db.Column(db.String(20), unique=False,
-                           nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=True)
 
     def __repr__(self):
