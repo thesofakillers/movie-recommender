acc812c81f5811827aa5656db50264ed20dd791c
Giulio Starace <giulio.starace@gmail.com>
2018-12-10 14:38:38 +0000

    create script to populate db from csv files and populate db

---
+++ csv_to_sql_helper.py
@@ -0,0 +1,45 @@
+from tabulate import tabulate
+from pellicola import db
+from pellicola.models import Rating, User, Movie, MovieGenre
+import pandas as pd
+
+engine = db.engine
+
+csv_folder = "ml-latest-small/"
+
+
+# read the movies csv file
+movies_df = pd.read_csv(csv_folder + "movies.csv")
+# read the ratings csv file
+ratings_df = pd.read_csv(csv_folder + "ratings.csv")
+
+# get rid of unnescessary column
+ratings_df = ratings_df.drop(columns=['timestamp'])
+# rename columns
+ratings_df.columns = ['user_id', 'movie_id', 'score']
+
+# get users dataframe
+users_df = ratings_df.user_id.drop_duplicates().to_frame('id')
+
+
+# create MovieGenre list of dics to then convert to pd dataframe
+movie_genre_list = [{'movie_id': row.movieId, 'genre': genre}
+                    for _, row in movies_df.iterrows()
+                    for genre in row.genres.split('|')]
+
+# MovieGenre dataframe equivalent, ready for sql
+movie_genre_df = pd.DataFrame(movie_genre_list)
+
+
+# rename movie df columns to match Movie SQL model column names
+movies_df.columns = Movie.__table__.columns.keys() + ['genres']
+# remove unnescessary column
+movies_df = movies_df.drop(columns=['genres'])
+
+
+# writing to database
+movies_df.to_sql('movie', con=engine, if_exists='append', index=False)
+movie_genre_df.to_sql('moviegenre', con=engine,
+                      if_exists='append', index=False)
+ratings_df.to_sql('rating', con=engine, if_exists='append', index=False)
+users_df.to_sql('user', con=engine, if_exists='append', index=False)
