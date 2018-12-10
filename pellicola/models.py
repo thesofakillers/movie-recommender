35dbb3d3705608658fb957a79c27c373a91018e6
Giulio Starace <giulio.starace@gmail.com>
2018-12-10 14:14:38 +0000

    fix database many to many...again

--- pellicola/models.py
+++ pellicola/models.py
@@ -10,20 +10,20 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-"""
-Handles the many to many relationship between users and films
-Each rating will mandatorily have
--a user who has placed the rating (FK) (PK)
--a movie which is being rated (FK) (PK)
--a score describing the value of the rating
-"""
-ratings = db.Table('ratings',
-                   db.Column('user_id', db.Integer, db.ForeignKey(
-                       'user.id'), primary_key=True),
-                   db.Column('movie_id', db.Integer, db.ForeignKey(
-                       'movie.id'), primary_key=True),
-                   db.Column('score', db.Float)
-                   )
+class Rating(db.Model):
+    """
+    Handles the many to many relationship between users and films
+    Each rating will mandatorily have
+    -a user who has placed the rating (FK) (PK)
+    -a movie which is being rated (FK) (PK)
+    -a score describing the value of the rating
+    """
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    movie_id = db.Column(db.Integer, db.ForeignKey(
+        'movie.id'), primary_key=True)
+    score = db.Column(db.Float)
+    user = db.relationship('User', back_populates='movies')
+    movie = db.relationship('Movie', back_populates='users')
 
 
 class User(db.Model, UserMixin):
@@ -32,15 +32,14 @@ class User(db.Model, UserMixin):
     Username/Email/passwords are optional since we are feeding the DB with a
     series of users which are missing this data
     Is related to:
-    -Movie: Many to Many handled via ratings
+    -Movie: Many to Many handled via Rating
     """
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=True)
     email = db.Column(db.String(254), unique=True, nullable=True)
     password = db.Column(db.String(60), nullable=True)
-    movies = db.relationship('Movie',
-                             secondary=ratings,
-                             back_populates='users')
+    movies = db.relationship('Rating',
+                             back_populates='user')
 
     def __repr__(self):
         """Define what the class should look like when printing out instance"""
@@ -52,14 +51,13 @@ class Movie(db.Model):
     Each movie will have a title and be part of one or more genres
     Is related to:
     -MovieGenre: 1 to M
-    -User: Many to Many handled via ratings
+    -User: Many to Many handled via Rating
     """
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     genres = db.relationship('MovieGenre', backref='movie', lazy='dynamic')
-    users = db.relationship("User",
-                            secondary=ratings,
-                            back_populates='movies')
+    users = db.relationship('Rating',
+                             back_populates='movie')
 
     def __repr__(self):
         """Define what the class should look like when printing out instance"""
